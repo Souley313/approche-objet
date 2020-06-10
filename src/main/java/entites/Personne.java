10136f6297ffe068e9e09d4736c478ad5254a527
Souleymane THIAM <souleymanethiam22@gmail.com>
2020-06-10 16:16:34 +0200

    TP3-INSTANCES ET METHODES

--- src/main/java/entites/Personne.java
+++ src/main/java/entites/Personne.java
@@ -1,23 +1,91 @@
 package entites;
-
-//Création de la classe personne
+/**
+ * @author Souleymane THIAM
+ * 
+ * Création de la classe personne
+ *
+ */
 public class Personne {
 
-		   String nom;
-	       String prenom;
-	public AdressePostale adr;
-	
-	//Un constucteur à la classe Personne avec les paramètres permettant de valorisez nom et prénom.
+	String nom;
+	String prenom;
+	AdressePostale adr;
+
+	/**
+	 * Un constucteur à la classe Personne avec les paramètres permettant de
+	 * valorisez nom et prénom.
+	 */
 	public Personne(String nom, String prenom) {
 		this.nom = nom;
 		this.prenom = prenom;
 	}
 
-	//Un constucteur à la classe Personne avec les paramètres permettant de valoriser les attributs nom, prenom et adresse
+	/**
+	 * Un constucteur à la classe Personne avec les paramètres permettant de
+	 * valoriser les attributs nom, prenom et adresse
+	 */
 	public Personne(String nom, String prenom, AdressePostale adr) {
 		this.nom = nom;
 		this.prenom = prenom;
 		this.adr = adr;
 	}
 
+	/**
+	 * Méthode permettant d’afficher le nom et le prénom avec le nom de famille en
+	 * majuscules
+	 */
+	public void afficheNomPrenom() {
+		System.out.println("Nom: " +nom.toUpperCase() + " Prénom: " +prenom);
+	}
+
+	/**
+	 * méthode qui prend un argument en paramètre et permet de modifier la variable
+	 * d’instance nom de Personne
+	 */
+	public void modifNom(String nvNom) {
+		nom = nvNom;
+	}
+
+	/**
+	 * méthode qui prend un argument en paramètre et permet de modifier la variable
+	 * d’instance prenom de Personne
+	 */
+	public void modifPrenom(String nvPrenom) {
+		prenom = nvPrenom;
+	}
+	
+	/**
+	 * méthode qui prend un argument en paramètre et permet de modifier l'adresse
+	 */
+	public void modifAdresse(int numRue, String libelleVoie, int codePostal, String ville) {
+		adr.numRue = numRue;
+		adr.libelleVoie = libelleVoie;
+		adr.codePostal = codePostal;
+		adr.ville = ville;
+	}
+	
+	/**
+	 * Méthode qui retourne le nom.
+	 */
+	public void afficheNom() {
+		System.out.println("Nom : " +nom);
+	}
+	
+	/**
+	 * Méthode qui retourne le prenom.
+	 */
+	public void affichePreom() {
+		System.out.println("Prenom : " +prenom);
+	}
+	
+	/**
+	 * Méthode qui retourne le prenom.
+	 * @return 
+	 */
+	public void afficheAdresse() {
+		
+		System.out.println("Adresse: " +adr.numRue  + adr.libelleVoie + adr.codePostal + adr.ville);
+	}
+	
+	
 }
--- src/main/java/essais/TestPersonne.java
+++ src/main/java/essais/TestPersonne.java
@@ -8,15 +8,26 @@ public class TestPersonne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Personne pers1 = new Personne("THIAM", "Souleymane");
+		Personne pers1 = new Personne(" THIAM ", "Souleymane");
 		
 		
 		
 		//Utilisation du second constructeur de la classe Personne.
-		AdressePostale adr = new AdressePostale (5, "des Maréchaux",44100,"Nantes");
-		Personne pers2 = new Personne("Bonnamy", "Richard",adr);
+		AdressePostale adr = new AdressePostale (5, " des Maréchaux ",44100," Nantes ");
+		Personne pers2 = new Personne(" Bonnamy ", " Richard ",adr);
 		//oubien pers2.adr = new AdressePostale(5, "des Maréchaux",44100,"Nantes");
-	
+		
+		pers2.afficheNomPrenom();
+		
+		System.out.println("\nAdresse avant modification :");
+		pers2.afficheAdresse();
+		
+		pers2.modifAdresse(55, " de Graville ", 76600, " Le Havre ");
+		
+		System.out.println("\nAdresse après modification :");
+		pers2.afficheAdresse();
+		
+		
 
 	}
 
