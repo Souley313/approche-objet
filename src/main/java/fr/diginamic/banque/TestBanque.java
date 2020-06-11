3102ef63128e5f2e8015983dff004eddfb58d2a2
Souleymane THIAM <souleymanethiam22@gmail.com>
2020-06-11 12:56:24 +0200

    TP1.J1-METTRE EN PLACE LES REGLES DE L’ENCAPSULATION

---
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package fr.diginamic.banque;
+
+import fr.diginamic.banque.entites.Compte;
+
+/**
+ * @author Souleymane THIAM
+ *
+ */
+public class TestBanque {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Compte compte1 = new Compte(1, 500); // Instanciation d'un compte
+		
+		System.out.println(compte1);
+
+	}
+}
---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package fr.diginamic.banque.entites;
+
+
+/**
+ * @author Souleymane THIAM
+ *
+ */
+public class Compte {
+	
+	private int numCompte; // numéro du compte
+	private double solde;  // solde du compte
+	/**
+	 * @return the numCompte
+	 */
+	public int getNumCompte() {
+		return numCompte;
+	}
+	/**
+	 * @param numCompte the numCompte to set
+	 */
+	public void setNumCompte(int numCompte) {
+		this.numCompte = numCompte;
+	}
+	/**
+	 * @return the solde
+	 */
+	public double getSolde() {
+		return solde;
+	}
+	/**
+	 * @param solde the solde to set
+	 */
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+	/**
+	 * @param numCompte
+	 * @param solde
+	 */
+	public Compte(int numCompte, double solde) {
+		super();
+		this.numCompte = numCompte;
+		this.solde = solde;
+	}
+	
+	
+}
