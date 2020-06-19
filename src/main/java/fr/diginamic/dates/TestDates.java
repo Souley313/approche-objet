e4266de3eb4d18cd2773dd25bc3d2362b4e6c7ba
Souleymane THIAM <souleymanethiam22@gmail.com>
2020-06-19 11:54:19 +0200

    TP1-J8-Date et Calendar.

--- src/main/java/fr/diginamic/dates/TestDates.java
+++ src/main/java/fr/diginamic/dates/TestDates.java
@@ -18,10 +18,12 @@ public class TestDates {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		@SuppressWarnings("deprecation")
 		Date date1 = new Date(120, 5, 19);
 		SimpleDateFormat format1 = new SimpleDateFormat("dd/MM/yyyy");
 		System.out.println(format1.format(date1));
 		
+		@SuppressWarnings("deprecation")
 		Date date2 = new Date(116, 4, 19, 23, 59, 30);
 		SimpleDateFormat format2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		System.out.println(format2.format(date2));
